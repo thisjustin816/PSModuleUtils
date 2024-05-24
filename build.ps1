49cf83ced168198537662475861c4a3a1e39faa5
Justin Beeson <justinbeeson@gmail.com>
2024-05-24 12:18:16 -0700

    build updates for v1.0 (#3)

--- build.ps1
+++ build.ps1
@@ -1,6 +1,6 @@
 $BuildPSModule = @{
     Name        = 'PSModuleUtils'
-    Version     = '0.0.5'
+    Version     = '1.0.0'
     Description = 'A module with helper functions to build and publish PowerShell modules to the PSGallery.'
     Tags        = ('PSEdition_Desktop', 'PSEdition_Core', 'Windows')
 }
