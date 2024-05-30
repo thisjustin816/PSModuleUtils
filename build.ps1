adac88b40d5d04c61a090eb39eb7f6a8c58a3b28
Justin Beeson <justinbeeson@gmail.com>
2024-05-30 12:10:06 -0700

    chore: Update PSModuleUtils version to 1.2.2 and add workflow_dispatch event

--- build.ps1
+++ build.ps1
@@ -1,6 +1,6 @@
 $BuildPSModule = @{
     Name        = 'PSModuleUtils'
-    Version     = '1.2.1'
+    Version     = '1.2.2'
     Description = 'A module with helper functions to build and publish PowerShell modules to the PSGallery.'
     Tags        = ('PSEdition_Desktop', 'PSEdition_Core', 'Windows')
 }
