66fdb6dfc0b57dba02e04afe32c04c39676c5b0e
Justin Beeson <justinbeeson@gmail.com>
2024-05-30 10:29:06 -0700

    chore: Bump PSModuleUtils version to 1.2.0

--- build.ps1
+++ build.ps1
@@ -1,6 +1,6 @@
 $BuildPSModule = @{
     Name        = 'PSModuleUtils'
-    Version     = '1.1.4'
+    Version     = '1.2.0'
     Description = 'A module with helper functions to build and publish PowerShell modules to the PSGallery.'
     Tags        = ('PSEdition_Desktop', 'PSEdition_Core', 'Windows')
 }
