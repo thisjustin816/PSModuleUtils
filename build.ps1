fe8bd91c18c722a6588e423e7583108cf61a35db
Justin Beeson <justinbeeson@gmail.com>
2024-12-20 08:40:18 -0800

    feat(Build-PSModule): :sparkles: Add optional GUID parameter to Build-PSModule function

--- build.ps1
+++ build.ps1
@@ -1,6 +1,6 @@
 $BuildPSModule = @{
     Name        = 'PSModuleUtils'
-    Version     = '1.7.1'
+    Version     = '1.7.2'
     Description = 'A module with helper functions to build and publish PowerShell modules to the PSGallery.'
     Tags        = ('PSEdition_Desktop', 'PSEdition_Core', 'Windows')
 }
